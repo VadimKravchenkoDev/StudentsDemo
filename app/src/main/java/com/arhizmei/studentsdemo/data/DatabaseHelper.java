5d8c813dc550d6e217bea7dd6ddb768c82c99538
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2025-08-28 22:21:57 +0300

    add getStudent

--- app/src/main/java/com/arhizmei/studentsdemo/data/DatabaseHelper.java
+++ app/src/main/java/com/arhizmei/studentsdemo/data/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.arhizmei.studentsdemo.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -20,10 +21,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String CREATE_STUDENT_TABLE = "CREATE TABLE " +
                 Util.TABLE_NAME + "(" +
-                Util.KEY_ID + " INTEGER PRIMARY KEY,"+
+                Util.KEY_ID + " INTEGER PRIMARY KEY," +
                 Util.TABLE_NAME + " TEXT," +
                 Util.KEY_SURNAME + " TEXT," +
-                Util.KEY_UNIVERSITY + " TEXT,"+
+                Util.KEY_UNIVERSITY + " TEXT," +
                 Util.KEY_AVERAGE_GRADE + " TEXT" + ")";
         db.execSQL(CREATE_STUDENT_TABLE);
 
@@ -31,11 +32,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(" DROP TABLE IF EXISTS "+ Util.TABLE_NAME);
+        db.execSQL(" DROP TABLE IF EXISTS " + Util.TABLE_NAME);
         onCreate(db);
     }
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         SQLiteDatabase database = this.getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
@@ -46,6 +47,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         database.insert(Util.TABLE_NAME, null, contentValues);
         database.close();
+    }
+
+    public Student getStudent(int id) {
+        SQLiteDatabase database = this.getReadableDatabase();
+
+        Cursor cursor = database.query(
+                Util.TABLE_NAME,
+                new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_SURNAME, Util.KEY_UNIVERSITY, Util.KEY_AVERAGE_GRADE},
+                Util.KEY_ID + "=?",
+                new String[]{String.valueOf(id)},
+                null, null, null);
+
+        Student student = null;
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                student = new Student(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getString(3),
+                        cursor.getString(4)
+                );
+            }
+            cursor.close();
+        }
+        database.close();
+        return student;
     }
 
 }
